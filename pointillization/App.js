120225455add9ce1fdf0648e4d41c09086c64064
harvey <harveydev24@gmail.com>
2022-03-19 06:34:44 +0900

    pointillization

---
+++ pointillization/App.js
@@ -0,0 +1,35 @@
+import { Point } from "./point.js";
+
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    this.point = new Point(this.stageWidth / 2, this.stageHeight / 2, 1000, 0);
+
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+  }
+
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+    this.point.animate(this.ctx, this.stageWidth, this.stageHeight);
+  }
+}
+
+window.onload = () => {
+  new App();
+};
---
+++ pointillization/point.js
@@ -0,0 +1,17 @@
+const PI2 = Math.PI2 * 2;
+
+export class Point {
+  constructor(destinationX, destinationY, radius, rgb) {
+    this.destinationX = destinationX;
+    this.destinationY = destinationY;
+    this.radius = radius;
+    this.rgb = rgb;
+  }
+
+  animate(ctx, stageWidth, stageHeight) {
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.arc(this.destinationX, this.destinationY, this.radius, 0, PI2, false);
+    ctx.fill();
+  }
+}
